ea94d9f9e5a54f3302c69f7a06695e6fb2fe44f8
Frank <frankyboy440@gmail.com>
2018-12-06 20:36:42 +0100

    Add code to sign in with google
    Adds support for android to ask user to sign in
    with google when launching the app for the first time,
    after doing it once the user doesn't have to do it again

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:YouAudio/playPage.dart';
 import 'package:YouAudio/searchPage.dart';
 import 'package:YouAudio/subscritonsPage.dart';
 import 'package:YouAudio/theme.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import "package:http/http.dart" as http;
 
 void main() {
   runApp(new MaterialApp(
@@ -22,11 +26,32 @@ class MyTabs extends StatefulWidget {
 
 class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
   TabController controller;
+  GoogleSignInAccount _currentUser;
+
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/youtube.readonly',
+    ],
+  );
 
   @override
   void initState() {
     super.initState();
+    _googleSignIn.signInSilently().then((GoogleSignInAccount account) {
+      if(account == null) {
+        _handleSignIn();
+      }
+    });
     controller = new TabController(vsync: this, length: 3);
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print("Something went wrong: $error");
+    }
   }
 
   @override
