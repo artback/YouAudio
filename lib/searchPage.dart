43279ee17c1035f8406a4cff28d4c031fec441b7
Frank <frankyboy440@gmail.com>
2018-12-19 23:36:20 +0100

    Seperated video and channel search
    Video tab searches for video while channel tab for channels.
    Made writing in the text field auto search

--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -1,14 +1,9 @@
 import 'package:YouAudio/theme.dart';
 import 'package:flutter/material.dart';
-
-//import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-//import 'dart:io';
 
-
-
-var apiKey   = "AIzaSyBKdwbjbsGdHyNPS0q3J6cffOsUSfiqCx4";
+var apiKey = "AIzaSyBKdwbjbsGdHyNPS0q3J6cffOsUSfiqCx4";
 
 class SearchList extends StatefulWidget {
   SearchList({Key key}) : super(key: key);
@@ -25,16 +20,19 @@ class _SearchListState extends State<SearchList>
   final key = new GlobalKey<ScaffoldState>();
   final TextEditingController _searchQuery = new TextEditingController();
   List<String> _list;
+  bool _value = false;
   String _searchText = "";
   TabController controller;
   Future<List> foundVideos;
+  Future<List> foundChannels;
 
   @override
   void initState() {
     super.initState();
     controller = new TabController(vsync: this, length: 3);
     init();
-    foundVideos = search(_searchText);
+    foundVideos = search(_searchText, "video");
+    foundChannels = search(_searchText, "channel");
   }
 
   _SearchListState() {
@@ -46,10 +44,13 @@ class _SearchListState extends State<SearchList>
       } else {
         setState(() {
           _searchText = _searchQuery.text;
+          foundVideos = search(_searchText, "video");
+          foundChannels = search(_searchText, "channel");
         });
       }
     });
   }
+
   void init() {
     _list = List();
     _list.add("Video 1");
@@ -67,60 +68,97 @@ class _SearchListState extends State<SearchList>
         body: new TabBarView(controller: controller, children: <Widget>[
           buildListView(),
           buildVideoList(), //View for searching youtube videos
-          buildListView()
+          buildChannelList()
         ]));
   }
 
   //builds view that present youtube videos
   Container buildVideoList() {
-    return new Container (
-      child: FutureBuilder<List>(
-          future: foundVideos,
-          builder: (context, snapshot) {
+    return new Container(
+        child: FutureBuilder<List>(
+            future: foundVideos,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List content = snapshot.data;
+                return new Container(
+                    child: new ListView.builder(
+                        itemCount: content.length,
+                        itemExtent: 150.0,
+                        itemBuilder: (BuildContext context, int index) {
+                          //some titles were extremely long so i made a quick fix
+                          if (content[index].title.length > 72) {
+                            content[index].title =
+                                content[index].title.substring(0, 72) + "...";
+                          }
+                          return ListTile(
+                              leading: Image.network(content[index].thumbnail),
+                              onLongPress: () {
+                                print("Open link to video " +
+                                    content[index].videoId);
+                              },
+                              title: RichText(
+                                  text: new TextSpan(
+                                text: '${content[index].title}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.black),
+                              )),
+                              trailing: IconButton(
+                                  icon: Icon(Icons.file_download),
+                                  onPressed: () {
+                                    //add download function here
+                                    print(
+                                        "downloading " + content[index].title);
+                                  }));
+                        }));
+              } else {
+                return Text("${snapshot.error}");
+              }
+              //return CircularProgressIndicator(); dont now why i cant place this here
+            }));
+  }
 
-            if (snapshot.hasData) {
-              List content = snapshot.data;
-              //print(content[0].title);
-              return new Container(
-                  child: new ListView.builder(
-                    itemCount: content.length,
-                    itemExtent: 150.0,
-                    itemBuilder: (BuildContext context, int index) {
-                      //some titles were extremly long so i made a quick fix
-                      if(content[index].title.length > 25) {
-                        content[index].title = content[index].title.substring(0, 25) + "...";
-                      }
+  void _valueChanged(bool value) => setState(() => _value = value);
 
-                      return ListTile(
-                        leading: Image.network(content[index].thumbnail),
-
-                        title: RichText(
-                            text: new TextSpan(
-                              text: '${content[index].title}',
-                              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: Colors.black),
-                            )
-                        ),
-
-                      trailing: IconButton(
-                          icon: Icon(Icons.file_download),
-                          onPressed: () {
-                            //add download function here
-                            print("downloading " + content[index].title);
+  //builds view that present youtube videos
+  Container buildChannelList() {
+    return new Container(
+        child: FutureBuilder<List>(
+            future: foundChannels,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List content = snapshot.data;
+                //print(content[0].title);
+                return new Container(
+                    child: new ListView.builder(
+                        itemCount: content.length,
+                        itemExtent: 150.0,
+                        itemBuilder: (BuildContext context, int index) {
+                          //some titles were extremly long so i made a quick fix
+                          if (content[index].title.length > 25) {
+                            content[index].title =
+                                content[index].title.substring(0, 25) + "...";
                           }
 
-                      )
-                      );
-                    }
-              )
-              );
-
-            } else {
-              return Text("${snapshot.error}");
-            }
-            //return CircularProgressIndicator(); dont now why i cant place this here
-          }
-      )
-    );
+                          return ListTile(
+                              leading: Image.network(content[index].thumbnail),
+                              title: RichText(
+                                  text: new TextSpan(
+                                text: '${content[index].title}',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.black),
+                              )),
+                              trailing: Checkbox(
+                                  value: _value, onChanged: _valueChanged));
+                        }));
+              } else {
+                return Text("${snapshot.error}");
+              }
+              //return CircularProgressIndicator(); dont now why i cant place this here
+            }));
   }
 
   ListView buildListView() {
@@ -168,7 +206,8 @@ class _SearchListState extends State<SearchList>
             _SearchListState();
             print(_searchText);
             setState(() {
-              foundVideos = search(_searchText);
+              foundVideos = search(_searchText, "video");
+              foundChannels = search(_searchText, "channel");
             });
           },
         ),
@@ -184,7 +223,9 @@ class _SearchListState extends State<SearchList>
 
 class ChildItem extends StatelessWidget {
   final String name;
+
   ChildItem(this.name);
+
   @override
   Widget build(BuildContext context) {
     //print(index);
@@ -195,26 +236,20 @@ class ChildItem extends StatelessWidget {
 //For searching youtube videos
 //q: search string
 //returns Future list of video classes from search result
-Future<List> search(String q) async {
-  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&maxResults=20&key=${apiKey}';
-  var response = await http.get(
-      Uri.encodeFull(url),
-      headers: {
-        "Accept": "application/json"
-      }
-  );
+Future<List> search(String search, String type) async {
+  var url =
+      'https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search}&maxResults=20&key=${apiKey}&type=${type}';
+  var response = await http
+      .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
 
   List<Video> videos = new List();
 
   if (response.statusCode == 200) {
     Map<String, dynamic> subsInfo = json.decode(response.body);
     List videoList = subsInfo['items'];
-    for(var x in videoList) {
-
-      videos.add(new Video(x["id"]["videoId"],
-          x["snippet"]["title"],
-          x["snippet"]["thumbnails"]["default"]["url"]
-      ));
+    for (var x in videoList) {
+      videos.add(new Video(x["id"]["videoId"], x["snippet"]["title"],
+          x["snippet"]["thumbnails"]["default"]["url"]));
       //print(x["id"]["videoId"]);
     }
     return videos;
@@ -229,10 +264,9 @@ class Video {
   String title;
   String thumbnail;
 
-  Video(this.videoId,
-      this.title,
-      this.thumbnail,
-      );
+  Video(
+    this.videoId,
+    this.title,
+    this.thumbnail,
+  );
 }
-
-
