50ac08d50366ab02d8ba4dff14fb4d6a3ddf3f62
moped <mos@mos.ase>
2018-12-19 18:52:42 +0000

    works for me :)

--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -1,6 +1,15 @@
 import 'package:YouAudio/theme.dart';
 import 'package:flutter/material.dart';
 
+//import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+//import 'dart:io';
+
+
+
+var apiKey   = "AIzaSyBKdwbjbsGdHyNPS0q3J6cffOsUSfiqCx4";
+
 class SearchList extends StatefulWidget {
   SearchList({Key key}) : super(key: key);
   @override
@@ -18,12 +27,14 @@ class _SearchListState extends State<SearchList>
   List<String> _list;
   String _searchText = "";
   TabController controller;
+  Future<List> foundVideos;
 
   @override
   void initState() {
     super.initState();
     controller = new TabController(vsync: this, length: 3);
     init();
+    foundVideos = search(_searchText);
   }
 
   _SearchListState() {
@@ -55,9 +66,61 @@ class _SearchListState extends State<SearchList>
         appBar: buildBar(context),
         body: new TabBarView(controller: controller, children: <Widget>[
           buildListView(),
-          buildListView(),
+          buildVideoList(), //View for searching youtube videos
           buildListView()
         ]));
+  }
+
+  //builds view that present youtube videos
+  Container buildVideoList() {
+    return new Container (
+      child: FutureBuilder<List>(
+          future: foundVideos,
+          builder: (context, snapshot) {
+
+            if (snapshot.hasData) {
+              List content = snapshot.data;
+              //print(content[0].title);
+              return new Container(
+                  child: new ListView.builder(
+                    itemCount: content.length,
+                    itemExtent: 150.0,
+                    itemBuilder: (BuildContext context, int index) {
+                      //some titles were extremly long so i made a quick fix
+                      if(content[index].title.length > 25) {
+                        content[index].title = content[index].title.substring(0, 25) + "...";
+                      }
+
+                      return ListTile(
+                        leading: Image.network(content[index].thumbnail),
+
+                        title: RichText(
+                            text: new TextSpan(
+                              text: '${content[index].title}',
+                              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15,color: Colors.black),
+                            )
+                        ),
+
+                      trailing: IconButton(
+                          icon: Icon(Icons.file_download),
+                          onPressed: () {
+                            //add download function here
+                            print("downloading " + content[index].title);
+                          }
+
+                      )
+                      );
+                    }
+              )
+              );
+
+            } else {
+              return Text("${snapshot.error}");
+            }
+            //return CircularProgressIndicator(); dont now why i cant place this here
+          }
+      )
+    );
   }
 
   ListView buildListView() {
@@ -97,6 +160,19 @@ class _SearchListState extends State<SearchList>
             hintText: "Search...",
             hintStyle: new TextStyle(color: Colors.white)),
       ),
+      //search button that recalls the youtube api and fetches result
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            _SearchListState();
+            print(_searchText);
+            setState(() {
+              foundVideos = search(_searchText);
+            });
+          },
+        ),
+      ],
       bottom: new TabBar(controller: controller, tabs: <Tab>[
         new Tab(text: 'Local'),
         new Tab(text: 'Videos'),
@@ -111,6 +187,52 @@ class ChildItem extends StatelessWidget {
   ChildItem(this.name);
   @override
   Widget build(BuildContext context) {
+    //print(index);
     return new ListTile(title: new Text(this.name));
   }
 }
+
+//For searching youtube videos
+//q: search string
+//returns Future list of video classes from search result
+Future<List> search(String q) async {
+  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&maxResults=20&key=${apiKey}';
+  var response = await http.get(
+      Uri.encodeFull(url),
+      headers: {
+        "Accept": "application/json"
+      }
+  );
+
+  List<Video> videos = new List();
+
+  if (response.statusCode == 200) {
+    Map<String, dynamic> subsInfo = json.decode(response.body);
+    List videoList = subsInfo['items'];
+    for(var x in videoList) {
+
+      videos.add(new Video(x["id"]["videoId"],
+          x["snippet"]["title"],
+          x["snippet"]["thumbnails"]["default"]["url"]
+      ));
+      //print(x["id"]["videoId"]);
+    }
+    return videos;
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
+
+// Holds important information of a video
+class Video {
+  String videoId;
+  String title;
+  String thumbnail;
+
+  Video(this.videoId,
+      this.title,
+      this.thumbnail,
+      );
+}
+
+
